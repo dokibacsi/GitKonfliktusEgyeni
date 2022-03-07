47bb4e1b790162a04ef3cd6dcedb08cd474391c5
dokibacsi <kvalcsi02@gmail.com>
2022-03-07 12:22:33 +0100

    3. név deklarálása

--- src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
+++ src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
@@ -6,6 +6,7 @@ public class EgyeniKonfliktusKezeles {
     public static void main(String[] args) {
         String nev1 = "KatoVale";
         String nev2 = "K.V";
+        String nev3;
     }
     
 }
