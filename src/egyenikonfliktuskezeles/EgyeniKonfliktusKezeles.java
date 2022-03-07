7b83423e3e71820484419315b4ace2b1cdf08a57
dokibacsi <94750684+dokibacsi@users.noreply.github.com>
2022-03-07 12:24:40 +0100

    Harmadik név deklarálása

--- src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
+++ src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
@@ -4,8 +4,10 @@ package egyenikonfliktuskezeles;
 public class EgyeniKonfliktusKezeles {
     
     public static void main(String[] args) {
+        int i = 7;
         String nev1 = "KatoVale";
         String nev2 = "K.V";
+        int nev3;
     }
     
 }
